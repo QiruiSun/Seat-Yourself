c5b7a85e8083b92d67476025b8e8df327a9d83dc
Franky Chau <houun.chau@gmail.com>
2016-02-04 15:03:21 -0500

    Fix input field for time, and successfully store it into database, and be able to show it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202192321) do
+ActiveRecord::Schema.define(version: 20160204193834) do
 
   create_table "reservations", force: :cascade do |t|
     t.datetime "time"
