b379dcac375d0064b4e99d0b15b569cacdc637e6
Qirui Sun <qiruisun@gmail.com>
2016-02-04 13:51:40 -0500

    Fix conflicts in model_restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,10 +3,9 @@ class Restaurant < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
   has_many :reservations
   has_many :users, through: :reservations
-<<<<<<< HEAD
+
   validates :name, :address, :price_range, :open_hour, :close_hour, presence: true
-=======
-  validates :name, :address, :price_range, :open_hour, :close_hour, :capacity, presence: true
+
 
   def open_seats?(date, time, requested_guests)
     counter = 0
@@ -18,5 +17,5 @@ class Restaurant < ActiveRecord::Base
   end
 
 
->>>>>>> be771c02a1e4da45d6c612684d7f9dd4eda7db1f
+
 end
