857f9e15beceb4d3ab26fc68298b6b8054d99fca
Franky Chau <houun.chau@gmail.com>
2016-02-05 15:38:03 -0500

    Add methods to add/remove points in reservation model, add views for points in reservation confirmation page, show points in user's profile page

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,20 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
+
+  after_create :add_user_points
+  after_destroy :remove_user_points
+
+  def remove_user_points
+    self.user.points -= self.party_size * 100
+    self.user.save
+  end
+
+  def add_user_points
+    # self.user.update_attributes(points: self.user.points + self.party_size * 10)
+    # self.user.points = self.user.points + self.party_size * 10
+    self.user.points += self.party_size * 100
+    self.user.save
+  end
+
 end
