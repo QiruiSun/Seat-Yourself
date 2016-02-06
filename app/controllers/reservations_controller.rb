3063e1a06493e493d198aada9dc45c6736fb4c5e
StuartSeupaul <stuartseupaul@gmail.com>
2016-02-06 15:10:14 -0500

    Reservation and restaurant controller now have documentation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,8 @@
 class ReservationsController < ApplicationController
 
+  # /users/:user_id/reservations(.:format)
   def index
-
+    # returns a collection of reservations if there is currently a user signed in
     @reservations = current_user.reservations
     # if current_user.owned_restaurants.any?
     #     @own_restaurant = current_user.owned_restaurants
@@ -12,10 +13,15 @@ class ReservationsController < ApplicationController
   def new
   end
 
+  # /restaurants/:restaurant_id/reservations(.:format)
   def create
+    # The reservation creation form is nested within a restaurant show page.
+    # First, the current restaurant page must be retrieved.
     @restaurant = Restaurant.find(params[:restaurant_id])
 
+    # If a user is logged in, the Restaurant controller -> show will give a reservation variable
     @reservation = @restaurant.reservations.build(reservation_params)
+    # Puts the current_user.id as the user_id in reservation because the form doesn't automatically do it
     @reservation.user = current_user
 
     # if open_seats?(reservation_params[:date], reservation_params[:time], reservation_params[:party_size]) != true
@@ -24,7 +30,8 @@ class ReservationsController < ApplicationController
     #   return
     # end
 
-
+    # if the save is successful then the user is directed to a list of all of their reservations
+    # otherwise the restaurant page is shown again
     if @reservation.save
       redirect_to user_reservations_path(current_user.id), notice: "Your reservation is confirmed!"
     else
@@ -35,6 +42,7 @@ class ReservationsController < ApplicationController
 
   end
 
+  # destroys the current reservation, then shows the user page which has a current list of reservations
   def destroy
     @reservation = Reservation.find(params[:id])
     @reservation.destroy
@@ -54,14 +62,13 @@ class ReservationsController < ApplicationController
   # @reservation = Reservation.find(params[:id])#code
   # end
 
+  # don't think this method is still being used
   def open_seats?(date, time, requested_guests)
     counter = 0
     @restaurant.reservations.where(date: date, time: time).find_each do |reservation|
       counter += reservation.party_size.to_i
     end
   end
-  
-
 
   private
   def reservation_params
